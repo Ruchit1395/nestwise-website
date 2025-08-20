b1d6a5b37b345e72baa1196e73dee0084d6b5dc1
Ruchit Dalwadi <ruchitdalwadi@192.168.1.13>
2025-08-20 15:31:12 +0530

    Fix scroll behavior: Add ScrollToTop component to automatically scroll to top on page navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import HomePage from './pages/HomePage';
 import FeaturesPage from './pages/FeaturesPage';
 import PricingPage from './pages/PricingPage';
@@ -14,6 +15,7 @@ import TermsOfServicePage from './pages/TermsOfServicePage';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen bg-white">
         <Header />
         <main>
